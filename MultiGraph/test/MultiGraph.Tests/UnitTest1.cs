9662fba28a40e1f94df5c3ca0e37f4f692840029
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2019-01-12 20:09:27 +0100

    added test projectws

---
+++ MultiGraph/test/MultiGraph.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiGraph.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
